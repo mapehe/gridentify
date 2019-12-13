707ecf07229c72ab54eec200e0a533d8d54c8220
Matias Heikkilä <matias@three.consulting>
2019-12-13 12:28:32 +0200

    Remove obsolete code

--- src/components/score_feed.js
+++ src/components/score_feed.js
@@ -10,64 +10,12 @@ import Noty from "noty"
 import "noty/lib/noty.css"
 import "noty/lib/themes/mint.css"
 
-class RealTimeScoreElement extends React.Component {
-  render() {
-    return (
-      <>
-        <p>
-          {this.props.username}: {this.props.score}
-        </p>
-      </>
-    )
-  }
-}
-
 class TopScoreElement extends React.Component {
   render() {
     return (
       <ListGroup.Item>
         {this.props.username} {this.props.score}
       </ListGroup.Item>
-    )
-  }
-}
-
-class RealTimeScoreFeed extends React.Component {
-  ref = null
-  constructor(props) {
-    super(props)
-    this.state = {
-      height: null,
-    }
-  }
-  componentDidMount() {
-    const height = this.ref.clientHeight
-    this.setState({
-      height: height,
-    })
-  }
-  render() {
-    return (
-      <>
-        <Card style={{ height: "100%" }}>
-          <Card.Header>Score Feed</Card.Header>
-          <Card.Body
-            ref={b => {
-              this.ref = b
-            }}
-            style={
-              this.state.height != null && !isMobile
-                ? { maxHeight: this.state.height, overlfow: "hidden" }
-                : isMobile
-                ? { height: "60vh" }
-                : {}
-            }
-            className="overflow-auto"
-          >
-            {this.props.children}
-          </Card.Body>
-        </Card>
-      </>
     )
   }
 }
@@ -89,7 +37,6 @@ class ScoreFeed extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      scores: [],
       daily_top: [...Array(5).keys()].map(e => {
         return { username: "", score: "0" }
       }),
