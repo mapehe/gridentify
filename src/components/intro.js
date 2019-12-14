0dc7626683af3b598888e0f61f2b49c812becaf8
Matias Heikkilä <matias@three.consulting>
2019-12-14 10:08:09 +0200

    Update intro text

--- src/components/intro.js
+++ src/components/intro.js
@@ -8,7 +8,7 @@ class Intro extends React.Component {
         <p className="lead mt-4 mb-5">
           Connect equal numbers in the grid below. The last number you select
           will be replaced by the sum of the connected numbers. The others will
-          be either 1, 2 or 3 at random.
+          replaced by either 1, 2 or 3 at random.
         </p>
       </div>
     )
