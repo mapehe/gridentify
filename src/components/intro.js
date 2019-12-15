993d50f7d9602fefa03e60aa550f514a833062e3
Matias Heikkilä <matias@three.consulting>
2019-12-15 17:24:36 +0200

    Favicon, modify SEO and ajust intro.

--- src/components/intro.js
+++ src/components/intro.js
@@ -9,7 +9,8 @@ class Intro extends React.Component {
         <p className="lead mt-4 mb-5">
           Connect equal numbers in the grid below. The last number you select
           will be replaced by the sum of the connected numbers. The others will
-          replaced by either 1, 2 or 3 at random.
+          replaced by either 1, 2 or 3 at random. The game will end when there
+          are no moves left.
         </p>
       </div>
     )
