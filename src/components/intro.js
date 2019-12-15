3b4e2e6b2e224b87d8106d6c88fc178730405682
Matias Heikkilä <matias@three.consulting>
2019-12-15 08:38:27 +0200

    Change domain

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,7 @@ class Intro extends React.Component {
   render() {
     return (
       <div>
-        <h1>Integer Snake</h1>
+        <h1 id="main_title">GRIDENTIO</h1>
         <p className="lead mt-4 mb-5">
           Connect equal numbers in the grid below. The last number you select
           will be replaced by the sum of the connected numbers. The others will
